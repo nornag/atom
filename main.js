76ff93f1a0d97d8f4ba416f6902b91d6e5d70c97
Nornag <nornag@gmal.com>
2015-05-20 18:31:55 +0900

    morning

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var API_KEY = window.GoogleSamples.Config.gcmAPIKey;
+var API_KEY = 'AIzaSyAnwknrCaXJiXGPrFUF2Okl40hAMDJOu5c';
 var GCM_ENDPOINT = 'https://android.googleapis.com/gcm/send';
 
 var curlCommandDiv = document.querySelector('.js-curl-command');
